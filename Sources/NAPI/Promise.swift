13e06efa982438feff5841e1b11d922b08b044e2
Greg Cotten <gregcotten@users.noreply.github.com>
2022-10-29 15:40:25 -0700

    Update Promise.swift

--- Sources/NAPI/Promise.swift
+++ Sources/NAPI/Promise.swift
@@ -1,37 +1,43 @@
 import NAPIC
 import Foundation
 
-class PromiseData {
-    var deferred: napi_deferred?
-    let args: Arguments
-    let function: AsyncCallback
-    var result: Result<ValueConvertible?, Swift.Error>?
-
-    init(args: Arguments, function: @escaping AsyncCallback) {
-        self.deferred = .init(bitPattern: 0)
-        self.args = args
-        self.function = function
+public class ThreadSafeDeferred {
+    private class Storage {
+        var result: Result<ValueConvertible, Swift.Error>?
     }
-}
 
-extension Task {
-    /// You better know what you're doing!
-    @discardableResult
-    func waitSync() -> Result<Success, Failure> {
-        let semaphore = DispatchSemaphore(value: 0)
+    private let finalize: ThreadsafeFunction
+    private(set) var promise: napi_value! = nil
+    private let storage: Storage
 
-        var res: Result<Success, Failure>?
-        let complete = { (result: Result<Success, Failure>) in
-            res = result
-            semaphore.signal()
-        }
+    public init(_ env: napi_env) throws {
+        var storage = Storage()
+        var deferred: napi_deferred! = nil
+        try napi_create_promise(env, &deferred, &promise).throwIfError()
 
-        Task<Void, Never>.detached {
-            complete(await self.result)
-        }
+        self.storage = storage
 
-        semaphore.wait()
+        finalize = try .init(env, Function(named: "ThreadSafeDeferred", { (env: napi_env) in
+            do {
+                let success = try storage.result!.get().napiValue(env)
+                try napi_resolve_deferred(env, deferred, success).throwIfError()
+            } catch {
+                try napi_reject_deferred(env, deferred, Value.undefined.napiValue(env)).throwIfError()
+            }
+        }))
+    }
 
-        return res!
+    func resolve(_ value: ValueConvertible) async throws {
+        guard storage.result == nil else { fatalError("tried resolving a finalized deferred object") }
+
+        storage.result = .success(value)
+        try await finalize.call()
+    }
+
+    func reject(_ error: Swift.Error) async throws {
+        guard storage.result == nil else { fatalError("tried rejecting a finalized deferred object") }
+
+        storage.result = .failure(error)
+        try await finalize.call()
     }
 }
