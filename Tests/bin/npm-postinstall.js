6666a2c957538f80f239e97b9bf08330e33b1f6f
Greg Cotten <greg@gregcotten.com>
2024-10-24 12:41:15 -0700

    oops

--- Tests/bin/npm-postinstall.js
+++ Tests/bin/npm-postinstall.js
@@ -13,7 +13,7 @@ async function run() {
   var libraryName = process.argv.slice(2)
   var swiftBuildCommand = "swift build -c release"
   var moveLibraryCommand = ""
-  var nodeHeadersVersion = "31.3.1"
+  var nodeHeadersVersion = "33.0.2"
   var downloadHeadersCommand = ""
 
   const exec = require("node:child_process").exec
